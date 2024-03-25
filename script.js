40393f86116d0b0af26e6a46055f4ec00d43651d
thrinkler <aeshadow0935@gmail.com>
2024-03-25 10:38:52 -0600

    Optimized a little bit

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let canvas = document.getElementById("myCanvas");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = canvas.height = 1000; 
 let ctx = canvas.getContext("2d");
 
-rule = "p ^ r"; // &, |, ^, ~ (alt+4 para el no)
+rule = " p|(q^r)"; // &, |, ^, ~ (alt+4 para el no)
 
 let getCascade = () => ({
-    size:1, //1,2,4,5,8,10,20,40,80... divisores
-    converter (p,q,r) {
-        return eval(rule);
-    },
+    size:10, // divisores
+    converter (p,q,r) {return eval(rule);},
     inst (table,index){
         a = [] //siempre la tabla anterior será de la anterior fila
 
         for(let b = 0; b < canvas.width/this.size;b++){
-            tf = this.converter(table.includes(b-1),table.includes(b),table.includes(b+1));
-            if (tf){
+            if (this.converter(table.includes(b-1),table.includes(b),table.includes(b+1))){
                 a.push(b)
                 ctx.fillRect(b*this.size,index*this.size,this.size,this.size)
             }
@@ -26,9 +22,8 @@ let getCascade = () => ({
 })
 
 Cascade = getCascade()
-
 firstPoint = canvas.width/(2*Cascade.size);
-tab = [firstPoint]
+tab = [firstPoint];
 ctx.fillRect(firstPoint*Cascade.size,0,Cascade.size,Cascade.size)
 for (let i = 1; i < canvas.height/Cascade.size; i++){
     tab = Cascade.inst(tab,i)
